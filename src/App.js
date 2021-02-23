22da537ba3910b9bddab05ef9c6b85cd92b50127
Matteo Zappalà <matteozappala@hotmail.it>
2021-02-23 16:21:50 +0100

    my first react project

--- src/App.js
+++ src/App.js
@@ -2,22 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const date = new Date;
+  const year = date.getFullYear();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main className="App-main">
+        <h1>My First React Application.</h1>
+      </main>
+      <footer className="App-footer">copyright by Me {year}</footer>
     </div>
   );
 }
