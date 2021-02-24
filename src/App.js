c258f578a0d7ee3627f1fb87605cbb0db64368a5
Matteo Zappalà <matteozappala@hotmail.it>
2021-02-24 18:51:33 +0100

    Add the bomb button and install consola

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import consola from 'consola';
 
 function App() {
-  const date = new Date;
-  const year = date.getFullYear();
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <main className="App-main">
-        <h1>My First React Application.</h1>
-      </main>
-      <footer className="App-footer">copyright by Me {year}</footer>
-    </div>
-  );
-}
+    const log = () => {consola.fatal('Boom!')}
+    const date = new Date;
+    const year = date.getFullYear();
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main className="App-main">
+          <h1>My First React Application.</h1>
+          <button onClick={log}>Bomb</button>
+        </main>
+        <footer className="App-footer">copyright by Me {year}</footer>
+      </div>
+    );
+  }
 
 export default App;
